c6083f06de4a07e95851b5844e07cf64471ab5ed
Mahmoud Tareq <mahmoudtarek.1335@gmail.com>
2023-07-16 15:32:28 +0300

    Add project files.

---
+++ STG1_D6/MCQ.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STG1_D6
+{
+    internal class MCQ:Questions
+    {
+        private string[] answers;
+        public string[] Answers
+        {
+              get { return answers; }
+            set { answers = value; }
+        }
+        #region contructors
+        public MCQ() : this(0)
+        {
+            Console.WriteLine("Mcq members(zero paramsters)");
+        }
+        public MCQ(int num) : this(num, null)
+        {
+            Console.WriteLine("Mcq members(one paramters)");
+        }
+        public MCQ(int num, string body) : this(num, body, 0)
+        {
+            Console.WriteLine("Mcq members(two paramters)");
+        }
+        public MCQ(int num , string body, int mark):base(num, body, mark) 
+        {
+            Console.WriteLine("Mcq members(three paramters)");
+        }
+        public MCQ(int num, string body, int mark,string[] answers) : base(num, body, mark)
+        {
+            Console.WriteLine("Mcq members(four paramters)");
+            Answers = answers;
+        }
+        #endregion
+        public override string show()
+        {
+
+            string result = $"Question number is {Num}\n" +
+               $"Question body is {Body}\n" +
+                $"Question mark is {Mark}\n" +
+                "Question answers are\n ";
+
+            if (answers != null)
+            {
+                for (int i = 0; i < Answers.Length; i++)
+                {
+                    result = result + Answers[i] + "  ";
+                }
+
+                
+            }
+             return result; 
+
+        }
+    }
+}
---
+++ STG1_D6/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace STG1_D6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MCQ q1 = new MCQ(1, "how many players in soccer team?", 15, new string[] { "10", "11", "12" });
+            Console.WriteLine( q1.show());
+
+            MCQ q2 = new MCQ(2);
+            q2.Body = "Who is the current prisdent of USA?";
+            q2.Mark = 5;
+            q2.Answers = new string[] { "biden", "tramp","obama" };
+            Console.WriteLine( q2.show());
+
+            TFQUESTIONS q3 = new TFQUESTIONS(3,"Egypt has 25 governorates",5);
+            Console.WriteLine(q3.show());
+
+
+        }
+    }
+}
